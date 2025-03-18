acc31e387b25895d0a7033f69ff5df80c06a801f
bea-de <beatriznevisnevis@gmail.com>
2025-03-18 18:52:08 -0300

    Update exemplo.py

--- Conceitos-Fundamentais /Alocação-Dinâmica/exemplo.py
+++ Conceitos-Fundamentais /Alocação-Dinâmica/exemplo.py
@@ -1,1 +1,12 @@
+def create_dynamic_list(n):
+dynamic_list = []
+para i em intervalo(n):
+dynamic_list.append(i)
+retornar dynamic_list
 
+# Tempo de execução
+n = int(input(“Digite o número de elementos: “))
+my_list = create_dynamic_list(n)
+
+# Tempo de compilação
+print(“Comprimento da lista:”, len(my_list))
