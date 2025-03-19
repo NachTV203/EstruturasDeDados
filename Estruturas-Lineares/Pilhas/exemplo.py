52f2abb9caf27a4582532b0a8a9ef9616f4096ae
Victor Alves <167913263+NachTV203@users.noreply.github.com>
2025-03-19 11:04:26 -0300

    Update exemplo.py

--- Estruturas-Lineares/Pilhas/exemplo.py
+++ Estruturas-Lineares/Pilhas/exemplo.py
@@ -1,1 +1,17 @@
+class Pilha:
+    def __init__(self):
+        self.itens = []
+
+    def empilhar(self, item):
+        self.itens.append(item)
+
+    def desempilhar(self):
+        return self.itens.pop() if self.itens else "Pilha vazia"
+
+# Exemplo de uso
+pilha = Pilha()
+pilha.empilhar(1)
+pilha.empilhar(2)
+pilha.empilhar(3)
+print(pilha.desempilhar())  # Saída: 3
 
