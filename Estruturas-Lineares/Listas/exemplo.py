fb1bd9a4057714377c6771b49eb904c4408e28a3
Victor Alves <167913263+NachTV203@users.noreply.github.com>
2025-03-19 14:11:59 -0300

    Update exemplo.py

--- Estruturas-Lineares/Listas/exemplo.py
+++ Estruturas-Lineares/Listas/exemplo.py
@@ -1,20 +1,20 @@
 #Exemplo de lista:
-# criando lista
+#criando lista
 a = [10, 20, "GfG", 40, True]
 
-# Mostra lista completa
+#Mostra lista completa
 print(a)
 
-# acessndo elementos expecificos da Lista
-print(a[0])  # 10
-print(a[1])  # 20
-print(a[2])  # "GfG"
-print(a[3])  # 40
-print(a[4])  # True
+#acessndo elementos expecificos da Lista
+print(a[0])  #10
+print(a[1])  #20
+print(a[2])  #"GfG"
+print(a[3])  #40
+print(a[4])  #True
 
-# exibe o tipo do elemento da lista
-print(type(a[2]))  # str
-print(type(a[4]))  # bool
+#exibe o tipo do elemento da lista
+print(type(a[2]))  #str
+print(type(a[4]))  #bool
 
 #saida
 #10, 20, 'GfG', 40, Verdadeiro]
