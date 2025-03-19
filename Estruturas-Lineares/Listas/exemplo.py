78b8c8280e9e08eabdb28747b52adc3e60638e36
Calanda01 <nomaya109@gmail.com>
2025-03-19 13:51:24 -0300

    CriandoLista.py

--- Estruturas-Lineares/Listas/exemplo.py
+++ Estruturas-Lineares/Listas/exemplo.py
@@ -1,1 +1,25 @@
+#Exemplo de lista:
+# criando lista
+a = [10, 20, "GfG", 40, True]
 
+# Mostra lista completa
+print(a)
+
+# acessndo elementos expecificos da Lista
+print(a[0])  # 10
+print(a[1])  # 20
+print(a[2])  # "GfG"
+print(a[3])  # 40
+print(a[4])  # True
+
+# exibe o tipo do elemento da lista
+print(type(a[2]))  # str
+print(type(a[4]))  # bool
+
+#saida
+#10, 20, 'GfG', 40, Verdadeiro]
+#10
+#20
+#GfG
+#40
+#Verdadeiro
