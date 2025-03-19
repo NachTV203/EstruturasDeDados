dbefd7620576dcbc121c8c9075fbbef5c038e447
Victor Alves <167913263+NachTV203@users.noreply.github.com>
2025-03-19 14:50:50 -0300

    Update Exemplo-Linear.py

--- Definição-Importância/Exemplo-Linear.py
+++ Definição-Importância/Exemplo-Linear.py
@@ -1,11 +1,13 @@
-# Exemplo de lista, uma estrutura linear
-# Vamos criar uma lista com alguns números
+#Exemplo de lista, uma estrutura linear
+
 numeros = [10, 20, 30, 40, 50]
 
-# Acessando os elementos da lista pelo índice
-print(numeros[0])  # Acessa o primeiro item (10)
-print(numeros[3])  # Acessa o quarto item (40)
+#Acessando os elementos da lista pelo índice
 
-# Adicionando um novo número no final da lista
+print(numeros[0])  #Acessa o primeiro item (10)
+print(numeros[3])  #Acessa o quarto item (40)
+
+#Adicionando um novo número no final da lista
+
 numeros.append(60)
-print(numeros)  # Agora a lista vai ser [10, 20, 30, 40, 50, 60]
+print(numeros)  #Agora a lista vai ser [10, 20, 30, 40, 50, 60]
